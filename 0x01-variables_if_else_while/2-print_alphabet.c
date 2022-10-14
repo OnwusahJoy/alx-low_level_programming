c463f6269a1e2ef8baeb93b82a3360ec575b5ce0
OnwusahJoy <ser.name>
2022-10-14 10:18:06 -0700

    Modified some files

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 {
 	char letter;
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Prints all the numbers of base 16 in lowercase.
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 {
 	int num;
