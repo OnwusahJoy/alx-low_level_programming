8271be220413b3005838905b45d37744ab383021
OnwusahJoy <ser.name>
2023-01-16 12:27:04 -0800

    Task 1

--- 0x18-dynamic_libraries/100-atoi.c
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -1,27 +1,34 @@
 #include "main.h"
 
 /**
- * _atoi - Converts a string to an integer.
- * @s: The string to be converted.
- *
- * Return: The integer value of the converted string.
+ * _atoi - a function that converts string to integer
+ * @s: An input string
+ * Return: integer from conversion
  */
 int _atoi(char *s)
 {
 	int sign = 1;
-	unsigned int num = 0;
+	unsigned int total = 0;
+	char null_flag = 0;
 
-	do {
+	while (*s)
+	{
 		if (*s == '-')
 			sign *= -1;
 
-		else if (*s >= '0' && *s <= '9')
-			num = (num * 10) + (*s - '0');
+		if (*s >= '0' && *s <= '9')
+		{
+			null_flag = 1;
+			total = total * 10 + *s - '0';
+		}
 
-		else if (num > 0)
+		else if (null_flag)
 			break;
+		s++;
+	}
 
-	} while (*s++);
+	if (sign < 0)
+		total = (-total);
 
-	return (num * sign);
+	return (total);
 }
--- 0x18-dynamic_libraries/3-puts.c
+++ 0x18-dynamic_libraries/3-puts.c
@@ -1,13 +1,19 @@
 #include "main.h"
 
 /**
- * _puts - Prints a string.
- * @str: string.
+ * _puts - Write a function that prints a string, followed
+ * by a new line, to stdout.
+ * @str: an input string
+ * Return: Nothing
  */
 void _puts(char *str)
 {
-	while (*str)
-		_putchar(*str++);
+	int i = 0;
 
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
 	_putchar('\n');
 }
